6b640fe3d447914aeef45484431ef9c16765ad57
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-01-18 13:35:11 -0800

    first

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# AzRenameBot
+## Rename Bot + File To Video Converter Bot 
+
+An Open Source Telegram Rename Bot With Permanent Thumbnail Support Also Have The Future To Convert Files Into Video 
+
+
+### Installation
+
+
+### You can tap the Deploy To Heroku button below to deploy straight to Heroku!
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/abdulhajiyev/AzRenameBot)
+
+
+## Credits, and Thanks to Beloved Developers ;
+
+* [SpEcHlDe](https://telegram.dog/SpEcHlDe) 
+* [Dan Tès](https://telegram.dog/haskell) 
+* [Yoily](https://telegram.dog/YoilyL)
+* [Anand](https://telegram.dog/Anandpskerala)
---
+++ translation.py
@@ -0,0 +1,37 @@
+class Translation(object):
+    START_TEXT = """ <b> Hi {} , I'm A Simple File Renamer+File To Video Converter Bot With Permanent Thumbnail support!💯</b> \n
+
+<b>Bot Maintained By: @abdulhajiyev</b> \n
+<b>Type /help for more...</b> \n
+"""
+
+    RENAME_403_ERR = "Wattafak Are You Doing?!!"
+    BANNED_USER_TEXT = "Sorry!! But my owner banned you from using this service. That means you can't run me now! \n \n Contact : @abdulhajiyev For more details.. " 
+    ABS_TEXT = "Wattafak are you trying to do,mate?"
+    UPGRADE_TEXT = "CONTACT @abdulhajiyev "
+    DOWNLOAD_START = "<b>Downloading to my server!! please wait</b>"
+    UPLOAD_START = "<b>Downloading completed now I'm uploading file to TG</b>"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF do you think! I'll upload It?"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for using me join > @AnonymousAzerbaijan  ❤️**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
+    NOT_AUTH_USER_TEXT = "CONTACT @abdulhajiyev "
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "If you got this message you should contact @abdulhajiyev "
+    SAVED_CUSTOM_THUMB_NAIL = "<b>Thumbnail saved ✅ this Is permanent until</b> /deletethumbnail ❤"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail cleared succesfully🤦"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "<b>File Downloaded successfully 😎</b>"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Please reply to an file with /rename \"filename.extension\" to rename a file</b>"
+    REPLY_TO_DOC_FOR_C2V = "<b> Please reply to an file with /c2v to convert it into streamable video file</b>"
+    REPLY_TO_DOC_FOR_C2A = "<b> Please reply to an file/video with /c2a to convert it into audio file</b>"
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No thumbnail found!🤒"
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    IFLONG_FILE_NAME = """You gotta be kidding me...Decrease the number of letters😆😉"""
+    ABOUT_ME = """<b>\nTelegram file renamer bot which can rename a telegram files. \n You can set permanent thumbnail for the file so you don't have to send custom thumbs all the time.\n \n I can also convert files into videos \n Join @AnonymousAzerbaijan if you found me useful</b>"""
+    HELP_USER = """Help menu
+    
+1.🔘 <b>Send or forward me any File.</b> \n
+2.🔘 <b>Reply to that message with /rename \"newfilename.extension\"</b> \n
+3.🔘 <b> Send a photo to make it as permanent Thumbnail</b> \n
+4.🔘 <b> I can also convert files into video through /c2v Command </b>   \n \n <b>Made with love by: @AnonymousAzerbaijan</b>"""
+
